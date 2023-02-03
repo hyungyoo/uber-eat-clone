7caa14b268667127830bf64663ec4a71d2876781
hyungyoo <hjyoo901112@gmail.com>
2023-02-03 19:45:45 +0100

    update: add validator for field of dto

--- backend/src/baseData/base.dto.ts
+++ backend/src/baseData/base.dto.ts
@@ -1,10 +1,13 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { IsBoolean, IsString } from "class-validator";
 
 @ObjectType()
 export class DisplayDtoResult {
   @Field((type) => Boolean)
+  @IsBoolean()
   isOk: boolean;
 
   @Field((type) => String, { nullable: true })
+  @IsString()
   errorMessage?: string;
 }
