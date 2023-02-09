b50595c29ea12f46551c12f88c4fe3d96b1fcab7
hyungyoo <hjyoo901112@gmail.com>
2023-02-09 03:05:29 +0100

    update: add issue

--- backend/src/users/entities/user.return.entiy.ts
+++ backend/src/users/entities/user.return.entiy.ts
@@ -1,19 +1,15 @@
 import { Field, ObjectType, PickType } from "@nestjs/graphql";
-import { IsEmail, IsEnum, IsString } from "class-validator";
 import { ParentEntity } from "src/baseData/base.entity";
 import { User, UserRole } from "./users.entity";
 
 @ObjectType()
 export class UserReturnType extends ParentEntity {
-  @IsEmail()
   @Field((type) => String)
   email: string;
 
-  @IsString()
   @Field((type) => String)
   name: string;
 
-  @IsEnum(UserRole)
   @Field((type) => UserRole)
   role: UserRole;
 }
