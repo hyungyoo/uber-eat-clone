3926d38fc148a078d09781aceb876d1758dd600f
hyungyoo <hjyoo901112@gmail.com>
2023-02-10 01:33:27 +0100

    update: for package.json, module name mapper

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,14 +1,12 @@
-import { Test } from "@nestjs/testing";
 import { UsersService } from "./users.service";
+import { Test } from "@nestjs/testing";
 
 describe("UsersService", () => {
   let UsersTestingService: UsersService;
-
   beforeAll(async () => {
     const UsersTestingModule = await Test.createTestingModule({
       providers: [UsersService],
     }).compile();
-
     UsersTestingService = UsersTestingModule.get<UsersService>(UsersService);
   });
 });
